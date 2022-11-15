8e0288a17e473aaab4a86d116ad4543b72890590
SooBin An <jejupalette@naver.com>
2022-11-15 18:42:30 +0900

    feat : 파일들 클릭 시 클릭에 맞는 데이터를 불러오도록 처리

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -1,3 +1,4 @@
+// 남은달 구하는 함수
 const remainMonthCalculate = (nowYear, nowMonth, startYear, startMonth, payMonth) => {
                             let remain
                             if (nowYear === startYear) {
@@ -10,128 +11,76 @@ const remainMonthCalculate = (nowYear, nowMonth, startYear, startMonth, payMonth
                             return remain
                         }
 
-
-$(function () {
-        $('.next').click(function () {
-            if (n_count == 0) {
-                $('.container').attr('style', 'transform: translate(-100vw)');
-                n_count = 1;
-            } else if (n_count == 1) {
-                $('.container').attr('style', 'transform: translate(-200vw)');
-                n_count = 2;
-            }
-        })
-    })
-
-$(function(){
-    $('.all-list-button').click(function (){
-        $('.tags').text('#결국 #다내꾸')
+const buttonClick = (select)=>{
+    select === 1 && $('.tags').text('#결국 #다내꾸')
+    select === 2 && $('.tags').text('#아주 #잠시 #은행꺼')
+    select === 3 && $('.tags').text('#넌 #내꺼야!')
         $('.image-container').empty();
             $.ajax({
                 type: "GET",
-                url: "/mainpage/items",
+                url: "/mainpage/getitems",
                 data: {},
+
                 success: function (response) {
                     let items = response["items"];
                     for (let i = 0; i < items.length; i++) {
-                        let img = items[i]['imgURL'];
+                        let img = items[i]['image'];
 
                         // 남은 달 구하기
-                        let startYear = items[i]["start_year"];
-                        let startMonth = items[i]["start_month"];
+                        let startDate = items[i]['date']
+                        let startYear = startDate.split('-')[0]
+                        let startMonth = startDate.split('-')[2]
                         let date = new Date
-                        let nowMonth = date.getMonth() +1
+                        let nowMonth = date.getMonth()+1
                         let nowYear = date.getFullYear()
-                        let payMonth = items[i]["pay_per_month"]
+                        let payMonth = items[i]["months"]
                         let remainMonth = remainMonthCalculate(nowYear, nowMonth, startYear, startMonth, payMonth)
 
-                        let temp_html = `<div class="image">
+                        let textRemainMonth = remainMonth === 0? '✔' : remainMonth
+
+                        let temp_html
+
+                        if(select === 1){temp_html = `<div class="image">
                                               <img
                                                 src=${img}
                                                 alt="사고싶은 물건"
                                               />
-                                              <div class="circle">${remainMonth}</div>
+                                              <div class="circle">${textRemainMonth}</div>
                                            </div>`
-                        $('.image-container').append(temp_html);
+                        $('.image-container').append(temp_html)}
+
+                        else if(select === 2 && remainMonth > 0){temp_html = `<div class="image">
+                                              <img
+                                                src=${img}
+                                                alt="사고싶은 물건"
+                                              />
+                                              <div class="circle">${textRemainMonth}</div>
+                                           </div>`
+                        $('.image-container').append(temp_html)}
+
+                        else if(select === 3 && remainMonth <= 0){temp_html = `<div class="image">
+                                              <img
+                                                src=${img}
+                                                alt="사고싶은 물건"
+                                              />
+                                              <div class="circle">${textRemainMonth}</div>
+                                           </div>`
+                        $('.image-container').append(temp_html)}
                     }
                 }
-            })
-    })
+            })}
 
+
+
+$(function(){
+    $('.all-list-button').click(()=>{buttonClick(1)})
 })
 
 $(function(){
-    $('.bank-list-button').click(function (){
-        $('.tags').text('#아주 #잠시 #은행꺼')
-        $('.image-container').empty();
-             $.ajax({
-                type: "GET",
-                url: "/mainpage/items",
-                data: {},
-                success: function (response) {
-                    let items = response["items"];
-                    for (let i = 0; i < items.length; i++) {
-                        let img = items[i]['imgURL'];
-
-                        // 남은 달 구하기
-                        let startYear = items[i]["start_year"];
-                        let startMonth = items[i]["start_month"];
-                        let date = new Date
-                        let nowMonth = date.getMonth() +1
-                        let nowYear = date.getFullYear()
-                        let payMonth = items[i]["pay_per_month"]
-                        let remainMonth = remainMonthCalculate(nowYear, nowMonth, startYear, startMonth, payMonth)
-                        let temp_html
-                        if(remainMonth > 0){temp_html = `<div class="image">
-                                              <img
-                                                src=${img}
-                                                alt="사고싶은 물건"
-                                              />
-                                              <div class="circle">${remainMonth}</div>
-                                           </div>`
-                        $('.image-container').append(temp_html);
-                        }
-                    }
-                }
-            })
-    })
+    $('.bank-list-button').click(()=>{buttonClick(2)})
 })
 
 $(function(){
-    $('.my-list-button').click(function (){
-        $('.tags').text('#넌 #내꺼야!')
-        $('.image-container').empty();
-              $.ajax({
-                type: "GET",
-                url: "/mainpage/items",
-                data: {},
-                success: function (response) {
-                    let items = response["items"];
-                    for (let i = 0; i < items.length; i++) {
-                        let img = items[i]['imgURL'];
-
-                        // 남은 달 구하기
-                        let startYear = items[i]["start_year"];
-                        let startMonth = items[i]["start_month"];
-                        let date = new Date
-                        let nowMonth = date.getMonth() +1
-                        let nowYear = date.getFullYear()
-                        let payMonth = items[i]["pay_per_month"]
-                        let remainMonth = remainMonthCalculate(nowYear, nowMonth, startYear, startMonth, payMonth)
-                        let temp_html
-
-                        if(remainMonth <= 0){temp_html = `<div class="image">
-                                              <img
-                                                src=${img}
-                                                alt="사고싶은 물건"
-                                              />
-                                              <div class="circle">${remainMonth}</div>
-                                           </div>`
-
-                       $('.image-container').append(temp_html);
-                    }
-                }
-            })
-    })
-
+    $('.my-list-button').click(()=>{buttonClick(3)})
 })
+
