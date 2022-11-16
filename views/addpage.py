302375f61aa79dc4b36853c6812dea61b052d31a
mugyeol <htpadkorik@naver.com>
2022-11-16 17:46:52 +0900

    아이템 추가 : jwt로 아이디 추출 및 db저장

--- views/addpage.py
+++ views/addpage.py
@@ -5,15 +5,21 @@ import requests
 from bs4 import BeautifulSoup
 import certifi
 from bson.objectid import ObjectId
+import jwt
 
-client = MongoClient('mongodb+srv://Luxury:hanghae99@luxury.uhfyrvo.mongodb.net/Luxury?retryWrites=true&w=majority',tlsCAFile=certifi.where())
+client = MongoClient(
+    'mongodb+srv://Luxury:hanghae99@luxury.uhfyrvo.mongodb.net/Luxury?retryWrites=true&w=majority', tlsCAFile=certifi.where())
 db = client.Luxury
 
 addpage = Blueprint("addpage", __name__, template_folder="templates")
 
+SECRET_KEY = 'SPARTA'
+
+
 @addpage.route('/')
 def home():
     return render_template('addpage.html')
+
 
 @addpage.route('/url', methods=["POST"])
 def get_url():
@@ -24,32 +30,45 @@ def get_url():
     data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
-    
-    price = soup.select_one('#content > div > div._2-I30XS1lA > div._2QCa6wHHPy > fieldset > div._1ziwSSdAv8 > div.WrkQhIlUY0 > div > strong > span._1LY7DqCnwR').text
+
+    price = soup.select_one(
+        '#content > div > div._2-I30XS1lA > div._2QCa6wHHPy > fieldset > div._1ziwSSdAv8 > div.WrkQhIlUY0 > div > strong > span._1LY7DqCnwR').text
     title = soup.select_one('meta[property="og:title"]')['content']
     image = soup.select_one('meta[property="og:image"]')['content']
 
-    return jsonify({ 'title': title,
-        'image': image,
-        'price': price})
-        
+    return jsonify({'title': title,
+                    'image': image,
+                    'price': price})
+
+
 @addpage.route('/adding', methods=["POST"])
 def item_post():
+    token_receive = request.cookies.get('mytoken')
+    print(token_receive)
     img_receive = request.form.get('img_give')
     title_receive = request.form.get('title_give')
-    price_receive  = int(request.form.get('price_give'))
-    date_receive = datetime.strptime(request.form.get('date_give'),'%Y-%m-%d')
+    price_receive = int(request.form.get('price_give'))
+    date_receive = datetime.strptime(request.form.get('date_give'), '%Y-%m-%d')
     months_receive = request.form.get('months_give')
-    
-    doc = {'email':"abc@naver.com",
-        'title': title_receive,
-        'image': img_receive,
-        'price': price_receive,
-        'date': date_receive,
-        'months':months_receive
-    }
-    print(doc)
-    db.items.insert_one(doc)
-    return jsonify({ 'msg': "success"})
-
-    
+    try:
+        # token을 시크릿키로 디코딩합니다.
+        # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+        doc = {'userid': payload['id'],
+               'title': title_receive,
+               'image': img_receive,
+               'price': price_receive,
+               'date': date_receive,
+               'months': months_receive
+               }
+        print(doc)
+        db.items.insert_one(doc)
+        return jsonify({'msg': "success"})
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        print("fail01")
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        print("fail02")
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
