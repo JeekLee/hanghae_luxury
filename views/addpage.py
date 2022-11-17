e29910eb073de5042f3ea70687cdb4ce973b70ba
mugyeol <htpadkorik@naver.com>
2022-11-17 13:20:25 +0900

    토큰 만료 시 리다이렉트 구현

--- views/addpage.py
+++ views/addpage.py
@@ -13,6 +13,7 @@ import jwt
 
 addpage = Blueprint("addpage", __name__, template_folder="templates")
 
+
 @addpage.route('/')
 def home():
     return render_template('addpage.html')
@@ -20,22 +21,30 @@ def home():
 
 @addpage.route('/url', methods=["POST"])
 def get_url():
+    token_receive = request.cookies.get('mytoken')
     url_receive = request.form.get('url_give')
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+    try:
+        payload = jwt.decode(token_receive, os.getenv(
+            'SECRET_KEY'), algorithms=['HS256'])
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        data = requests.get(url_receive, headers=headers)
 
-    soup = BeautifulSoup(data.text, 'html.parser')
+        soup = BeautifulSoup(data.text, 'html.parser')
 
-    price = soup.select_one(
-        '#content > div > div._2-I30XS1lA > div._2QCa6wHHPy > fieldset > div._1ziwSSdAv8 > div.WrkQhIlUY0 > div > strong > span._1LY7DqCnwR').text
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
+        price = soup.select_one(
+            '#content > div > div._2-I30XS1lA > div._2QCa6wHHPy > fieldset > div._1ziwSSdAv8 > div.WrkQhIlUY0 > div > strong > span._1LY7DqCnwR').text
+        title = soup.select_one('meta[property="og:title"]')['content']
+        image = soup.select_one('meta[property="og:image"]')['content']
 
-    return jsonify({'title': title,
-                    'image': image,
-                    'price': price})
+        return jsonify({"result": "success", 'data': {'title': title,
+                                                      'image': image,
+                                                      'price': price}})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
 @addpage.route('/adding', methods=["POST"])
@@ -50,7 +59,8 @@ def item_post():
     try:
         # token을 시크릿키로 디코딩합니다.
         # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
-        payload = jwt.decode(token_receive, os.getenv('SECRET_KEY'), algorithms=['HS256'])
+        payload = jwt.decode(token_receive, os.getenv(
+            'SECRET_KEY'), algorithms=['HS256'])
         print(payload)
         doc = {'userid': payload['id'],
                'title': title_receive,
@@ -62,8 +72,9 @@ def item_post():
                }
         print(doc)
         db.items.insert_one(doc)
-        return jsonify({'msg': "success"})
+        return jsonify({'result': "success"})
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
